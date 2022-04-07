db548bf81f5fbe3a40f43d718a816b8eafa21317
Ovidiu Goanta <Goantza_jr@yahoo.com>
2022-04-07 16:37:34 +0200

    new-mobile-version

--- script.js
+++ script.js
@@ -3,19 +3,43 @@ const asideAnimation = document.querySelector(".aside");
 const navElemente = document.querySelector(".nav-content");
 const navListe = document.querySelectorAll(".nav-list");
 let menuOpen = false;
+let ticking = false;
 
-menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    asideAnimation.classList.add("open");
-    navElemente.classList.add("open");
-    navListe.forEach((el) => el.classList.add("open"));
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove("open");
-    navListe.forEach((el) => el.classList.remove("open"));
-    navElemente.classList.remove("open");
-    asideAnimation.classList.remove("open");
-    menuOpen = false;
-  }
-});
+function openBurgerMenu() {
+  menuBtn.addEventListener("click", () => {
+    if (!menuOpen) {
+      menuBtn.classList.add("open");
+      asideAnimation.classList.add("open");
+      navElemente.classList.add("open");
+      navListe.forEach((el) => el.classList.add("open"));
+      menuOpen = true;
+      ticking = true;
+    } else {
+      menuBtn.classList.remove("open");
+      navListe.forEach((el) => el.classList.remove("open"));
+      navElemente.classList.remove("open");
+      asideAnimation.classList.remove("open");
+      menuOpen = false;
+    }
+  });
+}
+
+let lastKnownScrollPosition = 0;
+
+function scrollBarColor() {
+  document.addEventListener("scroll", function (e) {
+    lastKnownScrollPosition = window.scrollY;
+
+    window.requestAnimationFrame(function () {
+      if (lastKnownScrollPosition >= 300 && menuOpen === false) {
+        menuBtn.classList.add("filter");
+        ticking = false;
+      } else {
+        menuBtn.classList.remove("filter");
+        ticking = true;
+      }
+    });
+  });
+}
+scrollBarColor();
+openBurgerMenu();
